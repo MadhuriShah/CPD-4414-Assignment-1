e6eaf9c6a438199493395d51a77a0cac150d6a7b
C0648301 <C0648301@SB-N208-34.lambton.on.ca>
2015-01-14 15:13:37 -0500

    Created Order Class with getters, setters and constructors.

--- src/cpd/pkg4414/assignment/pkg1/Order.java
+++ src/cpd/pkg4414/assignment/pkg1/Order.java
@@ -6,10 +6,79 @@
 
 package cpd.pkg4414.assignment.pkg1;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author c0647610
  */
 public class Order {
+    private String customerId;
+    private String customerName;
+    private Date timeRecieved;
+    private Date timeProcessed;
+    private Date timeFulfilled;
+    private final List<Purchase> listOfPurchases = new ArrayList<>();
+    private String notes;
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public Date getTimeRecieved() {
+        return timeRecieved;
+    }
+
+    public void setTimeRecieved(Date timeRecieved) {
+        this.timeRecieved = timeRecieved;
+    }
+
+    public Date getTimeProcessed() {
+        return timeProcessed;
+    }
+
+    public void setTimeProcessed(Date timeProcessed) {
+        this.timeProcessed = timeProcessed;
+    }
+
+    public Date getTimeFulfilled() {
+        return timeFulfilled;
+    }
+
+    public void setTimeFulfilled(Date timeFulfilled) {
+        this.timeFulfilled = timeFulfilled;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Order(String customerId, String customerName, Date timeRecieved, Date timeProcessed, Date timeFulfilled, String notes) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.timeRecieved = timeRecieved;
+        this.timeProcessed = timeProcessed;
+        this.timeFulfilled = timeFulfilled;
+        this.notes = notes;
+    }
+    
     
 }
