2e53e80a7db938d4c63ccb740afa43bd90c0bd78
c0647754 <c0647754@SB-N208-33.lambton.on.ca>
2015-01-14 15:24:07 -0500

    Built the purchase class  with getters and setters and constructors.

--- src/cpd/pkg4414/assignment/pkg1/Purchase.java
+++ src/cpd/pkg4414/assignment/pkg1/Purchase.java
@@ -11,5 +11,28 @@ package cpd.pkg4414.assignment.pkg1;
  * @author C0648301
  */
 class Purchase {
+        private String product_id;
+    private int quantity;
+    
+    public Purchase (String product_id, int quantity){
+        this.product_id = product_id;
+        this.quantity = quantity;
+    }
+
+    public String getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(String product_id) {
+        this.product_id = product_id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
     
 }
