c9e5da902bbf7e8c1a8f3f83d8881d10952b2631
c0647456 <c0647456@SB-N208-35.lambton.on.ca>
2015-01-14 16:24:06 -0500

    Initial menu driven main program

--- src/cpd/pkg4414/assignment/pkg1/CPD4414Assignment1.java
+++ src/cpd/pkg4414/assignment/pkg1/CPD4414Assignment1.java
@@ -6,6 +6,10 @@
 
 package cpd.pkg4414.assignment.pkg1;
 
+import java.util.Scanner;
+
+
+
 /**
  *
  * @author c0647610
@@ -17,6 +21,20 @@ public class CPD4414Assignment1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int ch;
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.println("=====================");
+            System.out.println("1. New Order");
+            System.out.println("2. ");
+            System.out.println("-99. Exit");
+            System.out.println("=====================");
+            ch=sc.nextInt();
+            if(ch==1){
+                
+            }
+        }
+        while(ch!=-99);
     }
     
 }
