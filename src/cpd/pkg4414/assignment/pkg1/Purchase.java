54e76f610ed49f4044a7fcbd58b5d9ebebb13731
C0648301 <C0648301@SB-N208-34.lambton.on.ca>
2015-01-14 15:16:34 -0500

    Created Order Class with getters, setters and constructors.

---
+++ src/cpd/pkg4414/assignment/pkg1/Purchase.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cpd.pkg4414.assignment.pkg1;
+
+/**
+ *
+ * @author C0648301
+ */
+class Purchase {
+    
+}
